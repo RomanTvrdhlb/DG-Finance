3fc2f5805bd437219a133dd45e410abce37a4bbb
roman <romasaab@gmail.com>
2022-09-21 12:35:44 +0300

    add project

---
+++ source/js/_components.js
@@ -0,0 +1,14 @@
+import './components/sliders';
+// import './components/tabs';
+// import './components/galery';
+// import './components/filters';
+import './components/accordions';
+import './components/to-top';
+// import './components/select';
+import './components/row';
+// import './components/counter';
+import './components/anchor';
+// import './components/more-review';
+import './components/replaceEL';
+import './components/modals';
+
